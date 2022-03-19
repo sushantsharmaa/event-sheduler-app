32d8b88032593409a6f032b165ad813feb770690
Sushant Sharma <sushant.x01@gmail.com>
2022-03-19 09:07:20 +0530

    Create A Event Sheduler & String Reversal Application

---
+++ app.js
@@ -0,0 +1,67 @@
+// ---------------Server and Static Files------------------
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+// -------------------------DB-----------------------------
+
+mongoose.connect("mongodb+srv://sushantsharma:sushant1994@cluster0.pwuas.mongodb.net/shedule");
+
+const eventSchema = {
+  text: String,
+  dateTime: String
+};
+
+const Event = mongoose.model("event", eventSchema);
+
+//------------------GET---------------------
+
+
+app.get("/", async (req, res) => {
+  Event.find({}, (err, foundEvents) => {
+    res.render("index", {
+      eventList: foundEvents
+    });
+  });
+});
+
+// -----------------POST-------------------
+
+app.post("/", function(req, res) {
+  const text = req.body.text;
+  const time = req.body.dateTime;
+  let n = text.length;
+  let result = "";
+
+  for (let i = n - 1; i >= 0; i--) {
+    result += text[i];
+  }
+
+  const output = () => {
+    res.render("reverse", {
+      reverse: result
+    });
+  }
+
+  Event.findOne({
+    dateTime: time
+  }, function(err, event) {
+    const myTimeout = setTimeout(output, `${result.length * 1000}`);
+  });
+});
+
+// -----------------------------------------------------------------
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
